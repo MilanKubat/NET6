5d2cb26af7625247b65e75efb330bcb3b2d28390
MiK <MiK@NB-KUBAT>
2021-10-04 12:17:32 +0200

    2. cviceni

--- AhojSvete/AhojSvete/Program.cs
+++ AhojSvete/AhojSvete/Program.cs
@@ -9,11 +9,17 @@ namespace AhojSvete
             Console.WriteLine("Hello World!");
             Console.WriteLine();
             SoucetATisk(5, 46);
+            Pozdrav("Milane");
         }
 
         static void SoucetATisk (int a, int b)
         {
             Console.WriteLine("Součet čísla {0} a čísla {1} je {2}", a, b, a + b);
+        }
+
+        static void Pozdrav(string Jmeno)
+        {
+            Console.WriteLine($"Zdravím tě, {Jmeno}!");
         }
     }
 }
