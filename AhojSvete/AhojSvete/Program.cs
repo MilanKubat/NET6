39a780ea7b263e6d5d60d43c4222c37e5e201c9b
MiK <MiK@NB-KUBAT>
2021-10-04 12:02:44 +0200

    prvni cviceni

--- AhojSvete/AhojSvete/Program.cs
+++ AhojSvete/AhojSvete/Program.cs
@@ -8,6 +8,12 @@ namespace AhojSvete
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine();
+            SoucetATisk(5, 46);
+        }
+
+        static void SoucetATisk (int a, int b)
+        {
+            Console.WriteLine("Součet čísla {0} a čísla {1} je {2}", a, b, a + b);
         }
     }
 }
