f24b22a3b7babcd1f84861a7f9dd8ef9026bd5a2
MiK <MiK@NB-KUBAT>
2021-10-05 12:16:14 +0200

    bod

---
+++ AhojSvete/AhojSvete/Bod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhojSvete
+{
+    class Bod
+    {
+        //KONSTRUKTORY
+        
+        //ATRIBUTY
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        //METODY
+        public override string ToString()
+        {
+            return X + "\t" + Y;
+        }
+
+    }
+}
